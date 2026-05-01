2086e1bb66409b42ca905b32411f96f6850d43b2
evedataanalyst <evelynfr17@hotmail.com>
2026-04-30 21:43:05 -0500

    Update TableAnalysis.sql

--- DataAnalysis/Use Cases/TableAnalysis.sql
+++ DataAnalysis/Use Cases/TableAnalysis.sql
@@ -4,3 +4,10 @@ Question:
 How many customers are registered in the system?
 */
 select count(*) from customers;
+
+2️⃣ Total Number of Orders
+Question:
+How many orders exist in the database?
+*/
+select count (*) from orders
+
